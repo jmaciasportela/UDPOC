218c30e91b52a230c9fa18221ed213dda7b5a549
Jesus Macias Portela <jmacias@full-on-net.com>
2013-10-02 15:25:27 +0200

    POC to check availability of SO_REUSEADDRESS on UDP sockets

---
+++ broadcast.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+'''
+Created on 01/10/2013
+
+@author: jmacias
+'''
+
+# Send UDP broadcast packets
+
+PORT = 8500
+
+import sys, time
+from socket import *
+
+s = socket(AF_INET, SOCK_DGRAM)
+s.bind(('', 0))
+s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
+while 1:
+    data = repr(time.time()) + '\n'
+    s.sendto(data, ('<broadcast>', PORT))
+    print "SENT %s" % data
+    time.sleep(2)
+
